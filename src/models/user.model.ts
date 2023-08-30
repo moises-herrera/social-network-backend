d3bc4c339efabc2eec0740055ded0ecfc35fee34
moises-herrera <moisesherrera037@gmail.com>
2023-08-30 18:23:48 -0500

    chore: add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose';
+import { Role, User } from 'src/interfaces';
+
+const UserSchema = new Schema<User>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: [Role.User, Role.Admin],
+      required: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const UserModel = model('users', UserSchema);
+
+export default UserModel;
