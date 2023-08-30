addb561eaeba8ef9e623890b1e9af61315099c01
moises-herrera <moisesherrera037@gmail.com>
2023-08-30 18:03:07 -0500

    chore: add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import { router } from './routes';
+import dbConnect from './config/db';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+dbConnect().then(() => {
+  console.log('DB connected');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
