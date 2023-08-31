ae1cbd425b90286d4326da3c82517a797fa6f83b
moises-herrera <moisesherrera037@gmail.com>
2023-08-30 21:12:33 -0500

    chore: add middleware

---
+++ src/middleware/validate-fields.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+
+/**
+ * Validate the fields of a request.
+ *
+ * @param req The request object.
+ * @param res The response object.
+ * @param next The next function.
+ * @returns A response with the errors if there are any.
+ */
+export const validateFields = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void | Response<any, Record<string, any>> => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  return next();
+};
