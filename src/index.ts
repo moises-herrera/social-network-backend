b47b5fd87c259bd65ac33e65dcf57dcaa3c92e76
moises-herrera <moisesherrera037@gmail.com>
2023-09-10 18:56:53 -0500

    chore: add get redirection

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import express from 'express';
 import { router } from './routes';
 import dbConnect from './config/db';
 import { configCloudinary } from './config/cloudinary';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,6 +20,10 @@ configCloudinary();
 
 dbConnect().then(() => {
   console.log('DB connected');
+});
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
